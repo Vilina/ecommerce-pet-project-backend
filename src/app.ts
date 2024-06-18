fdfecc4a1cce3e7207dac86942189594fd057ef4
Vilina Osilova <vilina.osilova@jambit.com>
2024-06-18 15:33:20 +0400

    refactoring

--- src/app.ts
+++ src/app.ts
@@ -7,20 +7,22 @@ app.use(express.json());
 app.use(userRouter);
 const PORT = process.env.PORT || 8000;
 
-// Connect to the database
-connectToDatabase()
-    .then((result)=>{
-        // Start Express server
+
+// Function to start the Express server
+const startServer = async () => {
+    try {
+        await connectToDatabase();
         app.listen(PORT, () => {
             console.log(`Server is running on port=${PORT}`);
         });
+    } catch (error) {
+        console.error(`Failed to connect to database: ${error}`);
+        process.exit(1); // Exit the process with failure code
+    }
+};
 
-    })
-    .catch((err: Error) => {
-        console.error(`Failed to connect to database: ${err}`);
-    });
-
-
+// Start the server
+startServer();
 
 
 
