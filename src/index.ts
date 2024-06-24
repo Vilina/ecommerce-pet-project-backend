334dabfc23e9212cf99ff47b05ee4c63c487fe78
Vilina Osilova <vilina.osilova@jambit.com>
2024-06-24 16:23:21 +0400

    add url encoding

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import bodyParser from 'body-parser';
 
 const app :Express = express();
 app.use(bodyParser.json());
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", routes);
 const PORT: string | number = process.env.PORT || 8000;
 
