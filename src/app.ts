a8b0499015be3ae2336ef1f1a9c48b4753be95e0
rbarseghyan <Ruben.Barseghyan@partner.bmw.de>
2024-07-09 15:29:59 +0400

    Apply session store

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectToDatabase from "./db_connection/db_connect"
+import MONGODB_URI from './db_connection/dbConnectUri'
 import userRouter from './modules/users/routes/users'
 import authRouter from './modules/auth/routes/auth'
 import session from 'express-session';
@@ -7,6 +8,7 @@ import passport from 'passport'
 import Router from "express";
 import "./middleware/passport/strategies/local-strategy"
 import {setVisitedSession} from "./middleware/passport/authenticated";
+import MongoStore from "connect-mongo";
 const router = Router();
 
 
@@ -17,13 +19,13 @@ app.use(express.json());
 
 app.use(session({
     secret: 'secretsessiondev',
-    saveUninitialized: false,
-    resave: false,
+    saveUninitialized: true,
+    resave: true,
     cookie: {
         maxAge: 6000 * 60
-    }
+    },
+    store: MongoStore.create({ mongoUrl: MONGODB_URI }),
 }));
-
 
 
 app.use(passport.initialize());
