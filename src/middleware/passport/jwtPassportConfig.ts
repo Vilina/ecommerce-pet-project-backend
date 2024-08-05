788a3d9372d8b571b8a409d48e1ae847ffabd77c
rbarseghyan <Ruben.Barseghyan@partner.bmw.de>
2024-08-05 18:09:19 +0400

    Merge

--- src/middleware/passport/jwtPassportConfig.ts
+++ src/middleware/passport/jwtPassportConfig.ts
@@ -6,6 +6,7 @@ import {
 } from 'passport-jwt';
 import UserModel from '../../modules/users/model/UserModel';
 import UserDao from '../../modules/users/dao/UserDao';
+import config from '../../config';
 
 /**
  * Define the options for the JWT strategy.
@@ -17,7 +18,7 @@ import UserDao from '../../modules/users/dao/UserDao';
  */
 const opts: StrategyOptions = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'jwt-secret-key',
+  secretOrKey: config.jwt_secret_key,
 };
 
 /**
