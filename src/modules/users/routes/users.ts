9fe5d4aea885528732561c2f2cc7797399c98ab5
rbarseghyan <Ruben.Barseghyan@partner.bmw.de>
2024-08-06 16:00:34 +0400

    Revert for users

--- src/modules/users/routes/users.ts
+++ src/modules/users/routes/users.ts
@@ -1,18 +1,25 @@
 import { Router } from 'express';
 import * as UserController from '../../users/controller/userController';
+import { authenticateLocal } from '../../../middleware/passport/strategies/local/local-strategy';
+import { authorizeRoles } from '../../../middleware/passport/strategies/local/local-guards';
 
 const router = Router();
 
 // GET /users
-router.get('/users', UserController.getUsers);
+router.get(
+  '/users',
+  authenticateLocal,
+  authorizeRoles('admin'),
+  UserController.getUsers,
+);
 
 // GET /users/:id
-router.get('users/:id', UserController.getUserById);
+router.get('users/:id', authenticateLocal, UserController.getUserById);
 
 // PUT /users/:id
-router.put('/users/:id', UserController.updateUserById);
+router.put('/users/:id', authenticateLocal, UserController.updateUserById);
 
 // DELETE /users/:id
-router.delete('users/:id', UserController.deleteUserById);
+router.delete('users/:id', authenticateLocal, UserController.deleteUserById);
 
 export default router;
