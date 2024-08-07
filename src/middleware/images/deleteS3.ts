78b438bbf85c6077767c2c28942668ad0366ee38
Vilina Osilova <vilina.osilova@jambit.com>
2024-08-07 16:29:35 +0400

    Remove redundant try catch block, We are already handling thrown errors in the caller function

--- src/middleware/images/deleteS3.ts
+++ src/middleware/images/deleteS3.ts
@@ -6,33 +6,29 @@ import ProductDao from '../../modules/products/dao/ProductDao';
 import ProductModel from '../../modules/products/model/ProductModel';
 
 export const deleteImagesFromBucket = async (images: string[]) => {
-  try {
-    const results = await Promise.all(
-      images.map(async (key: string) => {
-        return await s3.send(
-          new DeleteObjectCommand({
-            Bucket: process.env.AWS_BUCKET_NAME,
-            Key: key,
-          }),
-        );
-      }),
-    );
-
-    // Check for any unsuccessful delete operations
-    const failedDeletes = results.filter(
-      (result) => result.$metadata.httpStatusCode !== 204,
-    );
-    if (failedDeletes.length > 0) {
-      throw new Error(
-        `Failed to delete some images: ${JSON.stringify(failedDeletes)}`,
+  const results = await Promise.all(
+    images.map(async (key: string) => {
+      return await s3.send(
+        new DeleteObjectCommand({
+          Bucket: process.env.AWS_BUCKET_NAME,
+          Key: key,
+        }),
       );
-    }
+    }),
+  );
 
-    console.log('Success. All images deleted.');
-    return results;
-  } catch (error: any) {
-    throw new Error(`Error deleting images: ${error.message}`);
+  // Check for any unsuccessful delete operations
+  const failedDeletes = results.filter(
+    (result) => result.$metadata.httpStatusCode !== 204,
+  );
+  if (failedDeletes.length > 0) {
+    throw new Error(
+      `Failed to delete some images: ${JSON.stringify(failedDeletes)}`,
+    );
   }
+
+  console.log('Success. All images deleted.');
+  return results;
 };
 
 const deleteImages: RequestHandler = async (req, res, next) => {
