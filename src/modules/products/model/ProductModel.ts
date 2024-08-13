495628168a81c76d51f3b17038f3a9736715fbd2
Vilina Osilova <vilina.osilova@jambit.com>
2024-08-13 14:40:33 +0400

    Add comments describing ProductModel.And update documentation regarding AWS S3 usage

--- src/modules/products/model/ProductModel.ts
+++ src/modules/products/model/ProductModel.ts
@@ -5,7 +5,7 @@ export interface IProduct extends Document {
   description: string;
   price: number;
   category: string;
-  imageKeys: string[];
+  imageKeys: string[]; // Array of keys that are saved in the S3 bucket
   color: string[];
   manufacturer: string;
   sale: boolean;
@@ -15,7 +15,7 @@ export interface IProduct extends Document {
   stock: number;
   createdAt: Date;
   updatedAt: Date;
-  imageUrls?: string[];
+  imageUrls?: string[]; // Array of full URLs to the images in the S3 bucket
 }
 
 const productSchema = new Schema<IProduct>(
@@ -24,7 +24,7 @@ const productSchema = new Schema<IProduct>(
     description: { type: String, required: true },
     price: { type: Number, required: true },
     category: { type: String, required: true },
-    imageKeys: { type: [String], required: true },
+    imageKeys: { type: [String], required: true }, // Array of keys that are saved in the S3 bucket
     color: { type: [String], required: false },
     manufacturer: { type: String, required: true },
     sale: { type: Boolean, required: true },
@@ -34,7 +34,7 @@ const productSchema = new Schema<IProduct>(
     stock: { type: Number, required: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
-    imageUrls: { type: [String], required: false },
+    imageUrls: { type: [String], required: false }, // Array of full URLs to the images in the S3 bucket
   },
   { timestamps: true },
 );
