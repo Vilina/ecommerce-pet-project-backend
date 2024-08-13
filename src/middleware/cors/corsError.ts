077e57a4c45808207a13113528a419706add6490
Vilina Osilova <vilina.osilova@jambit.com>
2024-08-13 18:30:52 +0400

    Fix Cors Error handling function

--- src/middleware/cors/corsError.ts
+++ src/middleware/cors/corsError.ts
@@ -1,10 +1,17 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 // Error handling middleware
-const corsError = (err: any, req: Request, res: Response) => {
+/* eslint-disable @typescript-eslint/no-unused-vars */ // we need this because https://github.com/expressjs/generator/issues/78#issuecomment-103152636
+const corsError = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
   if (err.message === 'Not allowed by CORS') {
     res.status(403).json({ error: 'CORS error: not allowed' });
   } else {
+    console.log(res, 'res');
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
